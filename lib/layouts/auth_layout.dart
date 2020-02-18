b44c66f819abf5995758227c642d43b2d55aba82
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-02-18 19:38:51 +0700

    tukar halaman dalam auth layout

--- lib/layouts/auth_layout.dart
+++ lib/layouts/auth_layout.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:states_rebuilder/states_rebuilder.dart';
 
 import '../pages/login_page.dart';
+import 'package:cupu/pages/register_page.dart';
+import 'package:cupu/stores/user_store.dart';
 
 class AuthLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ReactiveModel<UserStore> _userStore = Injector.getAsReactive<UserStore>(context: context);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
           padding: EdgeInsets.symmetric(horizontal: 25.0,),
-          child: LoginPage(),
+          child: _userStore.state.isRegister ? RegisterPage() : LoginPage(),
         ),
       ),
     );
